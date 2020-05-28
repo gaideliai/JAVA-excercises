a8f5cc4c525dde5c5e9ee4ca0dfa24ad2d4749f0
Anna <61122798+gaideliai@users.noreply.github.com>
2020-05-28 13:59:45 +0300

    Update copy-file.java

--- copy-file.java
+++ copy-file.java
@@ -1,16 +1,16 @@
-public static void copyfile(String[] args) throws FileNotFoundException, IOException {
-    List l = new ArrayList();
-    int b = 0;
-    try (FileInputStream fis = new FileInputStream("c.txt")) {
+public static void copyFile(String[] args) throws FileNotFoundException, IOException {
+    try (
+            FileInputStream fis = new FileInputStream("c.txt");
+            FileOutputStream fos = new FileOutputStream("d.txt");
+        ) {
+        int b = 0;
         while ((b = fis.read()) != -1) {
-            l.add(b);   
-        }            
-    }
-    try (FileOutputStream fos = new FileOutputStream("d.txt")) {   
-            for (int i = 0; i < l.size(); i++) {
-                int a = (int) l.get(i);
-                fos.write(a);                    
-            }
-            fos.flush();
+            fos.write(b);   
+        }
+        fos.flush();
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
 }
